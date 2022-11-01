0ed9a0e46bfc8c5b6a7ea20c5bd30c6b1bc374e1
tehranimilad <tehranimilad92@gmail.com>
2022-11-01 15:55:27 -0700

    added all my information, and everything is functional. Time to work my css magic.

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-const pages = document.getElementByTagName('section')
-const buttons = document.getElementByTagName('button')
+const linkedInButton = document.getElementById('linkedInButton')
+const gitHubButton = document.getElementByTagName('gitHubButton')
 
 
-function onClickHandle(event) {
-    event.target.innerText.toLowerCase().replaceAll(' ', '.')
+var img = document.createElement("img");
+img.src = "http://www.google.com/intl/en_com/images/logo_plain.png";
+var src = document.getElementById("header");
+src.appendChild(img);
 
-    for(let = 0; i< pages.length; i++) {
-        if(pageId === pages[i].id) {
-            pages[i].className = "current-page"
-        } else {
-            pages[i].className = "hidden-page"
-        }
-    }
-    document.getElementById(`page${}`).className = "current-page"
-    document.getElementById(`pages${}`).className = "hidden-page"
-}
 
-for (let i = 0, buttons.length, i++) {
-    buttons[i].addEventListener('click', onClickHandle)
-}
+// function linkedInLink() {
+
+// }
+document.getElementById('linkedInButton').addEventListener('click', linkedInButton);
