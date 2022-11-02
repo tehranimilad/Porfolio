3eaeaf4fc92f5fdb2896c5fccd98cd2903051147
tehranimilad <tehranimilad92@gmail.com>
2022-11-01 19:00:55 -0700

    last updates for tonight

--- app.js
+++ app.js
@@ -2,13 +2,6 @@ const linkedInButton = document.getElementById('linkedInButton')
 const gitHubButton = document.getElementByTagName('gitHubButton')
 
 
-var img = document.createElement("img");
-img.src = "http://www.google.com/intl/en_com/images/logo_plain.png";
-var src = document.getElementById("header");
-src.appendChild(img);
 
 
-// function linkedInLink() {
-
-// }
 document.getElementById('linkedInButton').addEventListener('click', linkedInButton);
