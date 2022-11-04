0b80f75628c2ff28d250cb8171adefe56982e36c
tehranimilad <tehranimilad92@gmail.com>
2022-11-04 12:56:09 -0700

    cleaning up my code and adding comments

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,10 +1,13 @@
+// About Me Page 
+// Creating variables for buttons displayed on about me page
 const pages = document.getElementsByTagName("section")
 const buttons = document.getElementsByTagName("button")
 
+// Creating the function that will show/hide specific content
 function buttonClick(event) {
     const pageId = event.target.innerText
 
-
+// Creating loop that selects page to display based on which button is clicked.
     for (i=0; i < buttons.length; i++) {
         if (pageId === pages[i].id) {
             pages[i].className = "current-page"
@@ -14,6 +17,7 @@ function buttonClick(event) {
     }
 }
 
+// Creating event listener that will wait for the button to be clicked
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', buttonClick)
 }
